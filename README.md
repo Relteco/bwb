cc2213d2ee982246e0ddc21b4b28f5a4d0371cd0
Relteco <opensource@relteco.com>
2022-05-12 04:31:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,127 @@
+
+# Relteco BWB
+## Bare Web Browser
+
+![]()
+
+
+A very simple web browser for usually i3wm,dwm,openbox of window manager users, and developers.
+It was designed mainly for use with a keyboard.
+
+## Preview 1
+![]()
+
+## Preview 2
+![]()
+
+
+# All Shortcuts
+
+## Fast Functions
+
+|  Shortcut   |  process 
+| -------------- | -------:|
+| `C+RETURN` | Open browser menu  |
+| `C+BACKSPACE` | Close browser menu  |
+| `C+K` | Prev page  |
+| `C+L` | Next page  |
+| `C+H` | Open home page  |
+| `C+J` | Open help page  |
+| `C+M+R` | Reload page  |
+
+## App
+
+|  Shortcut   |  process 
+| -------------- | -------:|
+| `C+Q` | kill BWB  |
+| `C+W` | Close BWB  |
+| `C+R` | Reload BWB  |
+| `C+M+R` | Force reload BWB  |
+| `C+M+I` | Toggle developer tools  |
+
+
+## View
+
+|  Shortcut   |  process 
+| -------------- | -------:|
+| `C+O` | Zoom reset  |
+| `C++` | Zoom in  |
+| `C+-` | Zoom out  |
+| `F11` | Toggle full screen  |
+| `C+M` | Minimize window |
+
+
+## Gloabal Edit
+
+|  Shortcut   |  process 
+| -------------- | -------:|
+| `C+Z` | Undo  |
+| `C+M+Z` | Redo  |
+| `C+X` | Cut  |
+| `C+C` | Copy  |
+| `C+Y` | Paste |
+| `DEL` | Delete  |
+| `C+A` | Select all  |
+
+
+----------------------
+
+# For Developers
+
+## How to build source code ?
+
+#### Download BWB project.
+
+ ```
+    $ git clone https://github.com/relteco/bwb.git
+```
+
+#### Go to project path
+
+ ``` 
+   $ cd bwb 
+```
+
+#### run npm installer 
+
+ ``` 
+   $ npm i
+```
+
+#### start project
+
+ ``` 
+   $ npm start
+```
+
+
+# Relteco Note
+
+hello dear fresh developer.
+Do you want to know how you can contribute to this project quickly?
+
+let's follow the steps
+
+- Join the project by clicking the "Fork" button first
+
+- Clone the project to your computer using Git.
+
+- Read the documents in the links below and update the "app.js" or "index.html" pages in the project to match the document.
+
+### Links
+
+[BrowserWindow | Electron](https://www.electronjs.org/docs/latest/api/browser-window)
+[Webview Tag | Electron](https://www.electronjs.org/docs/latest/api/webview-tag)
+[Keyboard Shortcuts | Electron](https://www.electronjs.org/docs/latest/tutorial/keyboard-shortcuts)
+
+
+- Finally, "pull request" to Relteco.
+
+congratulations !!
+ You are now an Electronjs developer who has joined the Relteco family.
+
+
+
+## License
+
+[MIT](https://github.com/relteco/bwb//LICENSE)
---
+++ app.js
@@ -0,0 +1,220 @@
+const { app, BrowserWindow, protocol, screen, Menu, ipcMain, url, webContents, BrowserView } = require('electron')
+
+const path = require('path'),
+  fs = require('fs');
+const homedir = app.getPath('home');
+
+
+let AppWin;
+const isMac = process.platform === 'darwin'
+
+
+
+// ** TR **
+// Electron projelerinde webview için klavye ve mouse dinlemeleri kapalıdır.
+// (webview.addEventListener("key##",()={}) NOT WORKING)
+// ayrıca bknz -> https://www.electronjs.org/docs/latest/api/webview-tag
+// > Internal implementation > You can not add keyboard, mouse, and scroll event listeners to webview.
+
+// bu yüzden klavye kısayollarında hiyeraşi sağlamanın en kolay yolu
+//menu içinde "Accelerator " özniteliğini kullanmaktır. 
+//bknz -> https://www.electronjs.org/docs/latest/tutorial/keyboard-shortcuts
+
+
+// ** EN **
+// In Electron projects, keyboard and mouse listening are turned off for webview.
+//(webview.addEventListener("key##",()={}) NOT WORKING)
+// see also  -> https://www.electronjs.org/docs/latest/api/webview-tag
+// > Internal implementation > You can not add keyboard, mouse, and scroll event listeners to webview.
+
+//so the easiest way to maintain hierarchy in keyboard shortcuts is to use the "Accelerator " attributein the menu.
+
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    label: 'Functions',
+    submenu: [
+      {
+        label: 'Open Menu',
+        accelerator: 'CmdOrCtrl+Return',
+        click: () => {
+          AppWin.webContents.send('key', '13')
+        }
+      },
+      {
+        label: 'Close Menu',
+        accelerator: 'CmdOrCtrl+Backspace',
+        click: () => {
+          AppWin.webContents.send('key', '8')
+        }
+      },
+      {
+        label: 'Prev Page',
+        accelerator: 'CmdOrCtrl+K',
+        click: () => {
+          AppWin.webContents.send('key', '75')
+        }
+      },
+      {
+        label: 'Next Page',
+        accelerator: 'CmdOrCtrl+L',
+        click: () => {
+          AppWin.webContents.send('key', '76')
+        }
+      },
+      {
+        label: 'Help Page',
+        accelerator: 'CmdOrCtrl+J',
+        click: () => {
+          AppWin.webContents.send('key', '74')
+        }
+      },
+      {
+        label: 'Reload Page',
+        accelerator: 'CmdOrCtrl+Alt+R',
+        click: () => {
+          AppWin.webContents.send('key', '82')
+        }
+      },
+      {
+        label: 'Home Page',
+        accelerator: 'CmdOrCtrl+H',
+        click: () => {
+          AppWin.webContents.send('key', '72')
+        }
+      }
+    ]
+  }
+
+]
+
+// menu oluştur
+// create custom menu
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+
+
+function initApp() {
+
+  AppWin = new BrowserWindow({
+    frame: false, // menubar ı gizle  // hide menubar
+    backgroundColor: "#101215",
+
+    webPreferences: { darkTheme: true, nodeIntegration: true, contextIsolation: false, webviewTag: true, devTools: true, preload: path.join(__dirname, 'preload.js') }
+  })
+
+  AppWin.loadFile('index.html')
+}
+
+
+
+
+
+
+
+
+
+
+
+app.whenReady().then(() => {
+  initApp()
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    initApp()
+  }
+})
+
+
+
